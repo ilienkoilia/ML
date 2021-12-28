434ba1a6d1e01eccef7f27cac6f83a4baca14f4d
ilienkoilia <96318653+ilienkoilia@users.noreply.github.com>
2021-12-28 12:03:57 +0300

    изменения по повышению точности

--- convnet.py
+++ convnet.py
@@ -15,48 +15,40 @@ class IntelBlock(nn.Module):
         '''
         super(IntelBlock, self).__init__()
         stride, dilation, padding = 1, 1, 1
-        
+
         self.module = nn.Sequential(
             nn.Conv2d(
-                in_channels, 
-                out_сhannels, 
-                kernel_size=3, 
-                stride=stride, 
-                dilation=dilation, 
+                in_channels,
+                out_сhannels,
+                kernel_size=3,
+                stride=stride,
+                dilation=dilation,
                 padding=padding
             ),
             nn.ReLU(),
-            nn.Conv2d(
-                out_сhannels, 
-                out_сhannels, 
-                kernel_size=1, 
-                stride=stride
-            ),
-            nn.BatchNorm2d(out_сhannels)
+            nn.BatchNorm2d(out_сhannels),
+            nn.MaxPool2d(2, 2)
         )
         self.relu = nn.ReLU()
 
         self.downsample = nn.Identity()
         if downsample:
             self.downsample = nn.Conv2d(
-                in_channels, 
-                out_сhannels, 
-                kernel_size=1, 
+                in_channels,
+                out_сhannels,
+                kernel_size=1,
                 stride=stride
             )
 
-
     def forward(self, input):
-        residual = input
-
         output = self.module(input)
-        output = self.relu(output + self.downsample(residual))
+        output = self.relu(output)
 
         return output
-        
+
 
 class IntelNet(nn.Module):
-    def __init__(self, out_dim=6, in_channels=3):
+    def __init__(self, out_dim=10, in_channels=3):
         '''
         out_dim: int,
             Number of classes in the target dataset. By default 6 for Intel dataset.
@@ -68,31 +60,31 @@ class IntelNet(nn.Module):
         depth_list = [32, 64, 128]
         self.out_shape = depth_list[-1]
 
-        self.extract_layer =  nn.Conv2d(
-            3, 
-            depth_list[0], 
-            kernel_size=7, 
-            stride=3, 
+        self.extract_layer = nn.Conv2d(
+            3,
+            depth_list[0],
+            kernel_size=3,
+            stride=1,
             dilation=1
         )
-        
-        self.block1 = IntelBlock(depth_list[0], depth_list[0])
-        self.block2 = IntelBlock(depth_list[0], depth_list[1], downsample=True)
-        self.block3 = IntelBlock(depth_list[1], depth_list[1])
-        self.block4 = IntelBlock(depth_list[1], depth_list[2], downsample=True)
+        self.blocks = nn.Sequential(
+            IntelBlock(depth_list[0], depth_list[0]),
+            IntelBlock(depth_list[0], depth_list[1], downsample=True),
+            IntelBlock(depth_list[1], depth_list[1]),
+            IntelBlock(depth_list[1], depth_list[2], downsample=True),
+            IntelBlock(depth_list[2], depth_list[2]),
+
+        )
 
         self.pooling = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(p=0.3)
         self.linear = nn.Linear(depth_list[-1], out_dim)
 
     def forward(self, x):
-
         output = self.extract_layer(x)
 
-        output = self.block1(output)
-        output = self.block2(output)
-        output = self.block3(output)
-        output = self.block4(output)
+        output = self.blocks(output)
 
-        output = self.pooling(output)
+        output = self.dropout(self.pooling(output))
 
         return self.linear(output.reshape(-1, self.out_shape))
